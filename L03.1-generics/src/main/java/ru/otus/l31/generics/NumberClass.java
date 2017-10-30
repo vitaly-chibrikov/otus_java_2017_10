0da7ab9dab28fd718b6ec2e9092dc4e97bec0a65
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2017-10-30 12:39:36 +0300

    Update NumberClass.java
    Number class in for Numbers

--- L03.1-generics/src/main/java/ru/otus/l31/generics/NumberClass.java
+++ L03.1-generics/src/main/java/ru/otus/l31/generics/NumberClass.java
@@ -3,8 +3,8 @@ package ru.otus.l31.generics;
 /**
  * Created by tully.
  */
-public class NumberClass extends GenericClass<String> {
+public class NumberClass extends GenericClass<Number> {
     public NumberClass(Number value) {
-        super("value");
+        super(value);
     }
 }
