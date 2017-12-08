5326fca20bb8eb2ac69e826eaf198edd45206f76
vitaly.chibrikov <vitaly.chibrikov@gmail.com>
2017-12-09 01:23:08 +0300

    IO and JSON

---
+++ L08.1-io/src/main/java/ru/otus/l81/io/Main.java
@@ -0,0 +1,52 @@
+package ru.otus.l81.io;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * Created by tully.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Student student = new Student(42, "tully", "java");
+        System.out.println(student);
+        String file = "student.out";
+
+        ioExample(student, file);
+
+        //apacheExample(student, file);
+
+        //String textFile = "hello.txt";
+        //String text = "Hello Java";
+        //writeTextFile(textFile, text);
+
+        //List<String> lines = Files.readAllLines(Paths.get(textFile));
+        //System.out.println(lines);
+
+    }
+
+    private static void writeTextFile(String textFile, String text) throws IOException {
+        byte[] textBytes = text.getBytes(Charset.forName("UTF-8"));
+        try (FileOutputStream stream = new FileOutputStream(textFile)) {
+            stream.write(textBytes);
+        }
+    }
+
+    private static void apacheExample(Student student, String file) throws IOException, ClassNotFoundException {
+        org.apache.commons.io.FileUtils.writeByteArrayToFile(new File(file), JavaIOHelper.serialize(student));
+        Student readStudent = (Student) JavaIOHelper.deserialize(FileUtils.readFileToByteArray(new File(file)));
+        System.out.println(readStudent);
+    }
+
+    private static void ioExample(Student student, String file) {
+        JavaIOHelper.writeObject(file, student);
+        Student readStudent = (Student) JavaIOHelper.readObject(file);
+        System.out.println(readStudent);
+    }
+
+
+}
